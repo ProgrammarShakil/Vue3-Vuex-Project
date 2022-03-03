49a9bfffaeac32fc1484072ef08a31414cbe9c91
shakil islam <freelancershakil913@gmail.com>
2022-03-03 22:28:36 +0600

    Project Starting Envionment Done

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Todo/>
   </div>
 </template>
 
 <script>
+import Todo from './components/Todo'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Todo
+  }
+
 }
 </script>
 
@@ -16,8 +20,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    To Do App With Vuex And Vue3
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
